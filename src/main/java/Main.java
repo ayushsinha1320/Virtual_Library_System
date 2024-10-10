68a78851bc91ee69d6920b91dec5e47645621e8a
Ayush Sinha <ayushs@testvagrant.com>
2024-10-10 13:43:08 +0530

    Designed data structure for books

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,45 @@
+import java.time.LocalDate;
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        LocalDate publication_date = LocalDate.of(2024,10,1);
+        Book b1 = new Book("Book1","John", 13, "Horror", publication_date,1);
+    }
+}
+
+class Book{
+    private final String Title;
+    private final String Author;
+    private final int ISBN;
+    private final String Genre;
+    private final LocalDate Publication_Date;
+    private final int NumberOfCopies;
+
+    public Book(String title, String author, int isbn, String genre, LocalDate publication_Date, int numberOfCopies){
+        this.Title = title;
+        this.Author = author;
+        this.ISBN = isbn;
+        this.Genre = genre;
+        this.Publication_Date = publication_Date;
+        this.NumberOfCopies = numberOfCopies;
+    }
+
+    public String getAuthor(){
+        return this.Author;
+    }
+
+    public String getTitle(){
+        return this.Title;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public String getGenre() {
+        return Genre;
+    }
+
+    public LocalDate getPublication_Date() {
+        return Publication_Date;
     }
 }
